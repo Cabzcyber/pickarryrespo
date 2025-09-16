23aa77bff6f3073af401dedd912ab1bee5d12509
Luke Cabatingan IT <142979792+Cabzcyber@users.noreply.github.com>
2025-09-16 14:25:04 +0800

    authlogsign progress

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+
 
 export default function RootLayout() {
-  return <Stack />;
+  return  (
+  <SafeAreaProvider>
+    <Stack  screenOptions={{headerShown:false}}>
+      <Stack.Screen name="index"/>
+    <Stack.Screen name="authlog"/>
+    <Stack.Screen name="authsign"/>
+    <Stack.Screen name="otp"/>
+    <Stack.Screen name="terms"/>
+    </Stack>
+  </SafeAreaProvider>
+
+
+      )
 }
